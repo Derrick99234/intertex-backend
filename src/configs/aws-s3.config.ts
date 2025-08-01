1e3d6322fe7c43dd80abb8e21a95e5d961723b1f
Derrick <pshubomi@gmail.com>
2025-08-01 12:59:23 +0100

    Fix: Removed AWS secrets from code and used env variables

---
+++ src/configs/aws-s3.config.ts
@@ -0,0 +1,27 @@
+import { v4 as uuidv4 } from 'uuid';
+import { S3Client } from '@aws-sdk/client-s3';
+import * as multerS3 from 'multer-s3';
+import * as dotenv from 'dotenv';
+dotenv.config();
+const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
+const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
+const region = process.env.AWS_REGION;
+const s3 = new S3Client({
+  region: 'eu-north-1',
+  credentials: { accessKeyId, secretAccessKey },
+});
+
+export const awsOption = {
+  storage: multerS3({
+    s3,
+    bucket: 'intertex-storage',
+    acl: 'public-read',
+    metadata: (req, file, cb) => {
+      cb(null, { fieldName: file.fieldname });
+    },
+    key: (req, file, cb) => {
+      const ext = file.originalname.split('.').pop();
+      cb(null, `products/${uuidv4()}.${ext}`);
+    },
+  }),
+};
