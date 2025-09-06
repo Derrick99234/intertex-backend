ddbc785ad39d7cfea590e5e79745d11e706795a7
Derrick <pshubomi@gmail.com>
2025-09-06 17:17:15 +0100

    :

--- src/main.ts
+++ src/main.ts
@@ -27,6 +27,7 @@ async function bootstrap() {
   // const userService = app.get(UserService);
   // await userService.generateFakeUsers(100);
 
-  await app.listen(process.env.PORT || 3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
