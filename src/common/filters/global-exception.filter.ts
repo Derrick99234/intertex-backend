d24f136dfdbabd14fc8b0ccaf85af6787f12c160
Deritech <pshubomi@gmail.com>
2026-06-08 12:07:55 +0100

    fix GlobalExceptionFilter crash: drop BaseExceptionFilter inheritance to avoid undefined applicationRef

--- src/common/filters/global-exception.filter.ts
+++ src/common/filters/global-exception.filter.ts
@@ -1,25 +1,27 @@
 import {
+  ExceptionFilter,
   Catch,
   ArgumentsHost,
   HttpException,
   HttpStatus,
   Logger,
 } from '@nestjs/common';
-import { BaseExceptionFilter } from '@nestjs/core';
 
 @Catch()
-export class GlobalExceptionFilter extends BaseExceptionFilter {
+export class GlobalExceptionFilter implements ExceptionFilter {
   private readonly logger = new Logger(GlobalExceptionFilter.name);
 
   catch(exception: unknown, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse();
+
     if (exception instanceof HttpException) {
-      return super.catch(exception, host);
+      const status = exception.getStatus();
+      const errorResponse = exception.getResponse();
+      return response.status(status).json(errorResponse);
     }
 
     this.logger.error('Unhandled exception', exception);
-
-    const ctx = host.switchToHttp();
-    const response = ctx.getResponse();
 
     response.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
       statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
